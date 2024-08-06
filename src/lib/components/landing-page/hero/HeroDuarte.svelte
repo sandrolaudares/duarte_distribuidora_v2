4f25e33cc95d9087e20b4f7283d85886ff74b7e5
Breno Brandão <brenoca35@gmail.com>
2024-08-06 14:37:25 -0300

    ignore+ tela inicial animation

--- src/lib/components/landing-page/hero/HeroDuarte.svelte
+++ src/lib/components/landing-page/hero/HeroDuarte.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex min-h-[80vh] flex-col items-center justify-center">
 	<img src={logo} alt="Logo" class="mb-3 max-h-60 max-w-60" />
-	<h1 class="mb-4 text-4xl font-bold text-center">Bem-vindo ao Sistema do Duarte</h1>
+	<h1 class="mb-4 text-4xl font-bold text-center inline-flex animate-text-gradient bg-gradient-to-r from-[#917e4e] via-[#000000] to-[#917e4e] bg-[200%_auto] text-transparent bg-clip-text">Bem-vindo ao Sistema do Duarte</h1>
 	<div class="flex flex-col lg:flex-row gap-4">
 		<a
 			href="/"
