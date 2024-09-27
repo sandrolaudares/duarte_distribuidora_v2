9118dc0c7fc607444e1439739b5f3686c5473e82
Breno Brandão <brenoca35@gmail.com>
2024-09-26 21:32:50 -0300

    search motoboy e busca nao encontrada

--- src/routes/admin/cashier/[id]/ModalCliente.svelte
+++ src/routes/admin/cashier/[id]/ModalCliente.svelte
@@ -126,8 +126,32 @@
       </div>
     {/if}
 
-    {#if searchTerm && clientes.length === 0}
-      <p class="text-center">Nenhum cliente com esse nome foi encontrado</p>
+    {#if searchTerm && filteredClientes.length === 0}
+    <div class="mt-6 flex min-h-40 items-center rounded-lg text-center">
+      <div class="mx-auto flex w-full max-w-sm flex-col px-4">
+        <div class="mx-auto rounded-full bg-base-200 p-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </div>
+        <h1 class="mt-3 text-lg font-semibold">Nenhum cliente encontrado</h1>
+        <p class="mt-2">
+          Sua busca não foi encontrada. Por
+          favor tente novamente.
+        </p>
+      </div>
+    </div>
     {:else}
       {#each filteredClientes as cliente}
         <div class="flex items-center justify-between">
--- src/routes/admin/cashier/[id]/ModalMotoboy.svelte
+++ src/routes/admin/cashier/[id]/ModalMotoboy.svelte
@@ -10,6 +10,7 @@
   type Motoboy = RouterOutputs['auth']['getMotoboys']
 
   export let motoboys: Motoboy = []
+  let filteredMotoboys = motoboys
 
   let isLoading = false
 
@@ -17,6 +18,7 @@
     try {
       isLoading = true
       motoboys = await trpc($page).auth.getMotoboys.query()
+      filteredMotoboys = motoboys
       console.log(motoboys)
       isLoading = false
     } catch (error: any) {
@@ -26,29 +28,89 @@
     }
   })
 
+  let searchTerm = ''
+
+  const searchMotoboys = () => {
+    return (filteredMotoboys = motoboys.filter(motoboy => {
+      let nome = motoboy.username.toLowerCase()
+      return nome.includes(searchTerm.toLowerCase())
+    }))
+  }
+
   export let selectedMotoboy: (motoboy: Motoboy[0]) => void
 </script>
 
 <Modal title="Motoboys">
   <div class="my-4 flex flex-col gap-4">
+    <label class="input input-bordered flex items-center gap-2">
+      <input
+        type="text"
+        class="grow"
+        placeholder="Search"
+        bind:value={searchTerm}
+        on:input={searchMotoboys}
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill="currentColor"
+        class="h-4 w-4 opacity-70"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+    </label>
     {#if isLoading}
-    <div class="flex justify-center items-center">
-      <Loading/>
-    </div>
-    {/if}
-    {#each motoboys as motoboy}
-      <div class="flex flex-col gap-3 rounded-box bg-base-200 p-4 text-center">
-        {motoboy.username} - {motoboy.email}
-        <button
-          class="btn btn-primary"
-          on:click={() => {
-            selectedMotoboy(motoboy)
-            modal.close()
-          }}
-        >
-          selecione
-        </button>
+      <div class="flex items-center justify-center">
+        <Loading />
       </div>
-    {/each}
+    {/if}
+    {#if searchTerm && filteredMotoboys.length === 0}
+    <div class="mt-6 flex min-h-40 items-center rounded-lg text-center">
+      <div class="mx-auto flex w-full max-w-sm flex-col px-4">
+        <div class="mx-auto rounded-full bg-base-200 p-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </div>
+        <h1 class="mt-3 text-lg font-semibold">Nenhum motoboy encontrado</h1>
+        <p class="mt-2">
+          Sua busca não foi encontrada. Por
+          favor tente novamente.
+        </p>
+      </div>
+    </div>
+    {:else}
+      {#each filteredMotoboys as motoboy}
+        <div
+          class="flex flex-col gap-3 rounded-box bg-base-200 p-4 text-center"
+        >
+          {motoboy.username} - {motoboy.email}
+          <button
+            class="btn btn-primary"
+            on:click={() => {
+              selectedMotoboy(motoboy)
+              modal.close()
+            }}
+          >
+            selecione
+          </button>
+        </div>
+      {/each}
+    {/if}
   </div>
 </Modal>
