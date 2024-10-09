ae9480a71a175c7c366360b7e0cfb6461bfae729
Breno Brandão <brenoca35@gmail.com>
2024-10-09 15:53:46 -0300

    function to new teble

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -9,3 +9,18 @@ export * from './image'
 export * from './icons'
 export * from './device'
 export * from './entities'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: number | undefined
+
+  return (...args: Parameters<T>): void => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = window.setTimeout(() => func(...args), delay)
+  }
+}
