d86767192d93aed0599a19268aa8b2c69894c22e
Breno Brandão <brenoca35@gmail.com>
2024-10-11 08:28:53 -0300

    currency + shownull

---
+++ src/lib/components/newTable/Currency.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let value = 0
+</script>
+
+<span class="font-bold">
+    R${(value/100).toFixed(2)}
+</span>
---
+++ src/lib/components/newTable/ShowNull.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let value
+</script>
+
+{#if value === null || !value}
+  <span class="text-error">Não tem</span>
+{:else}
+  {value}
+{/if}
