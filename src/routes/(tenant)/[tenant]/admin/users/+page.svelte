110a22176f786aafce05d4f64276b2936370bc34
brenin35 <brenoca35@gmail.com>
2024-10-23 15:42:29 -0300

    new datatable first table users

--- src/routes/(tenant)/[tenant]/admin/users/+page.svelte
+++ src/routes/(tenant)/[tenant]/admin/users/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<!-- <script lang="ts">
   import type { PageData } from './$types'
 
   let { data }: { data: PageData } = $props()
@@ -278,4 +278,92 @@
         }),
     }),
   ]}
-/>
+/> -->
+
+<script lang="ts">
+  import { navigating } from '$app/stores'
+  import { SSRFilters } from '$lib/components/datatable/filter.svelte'
+  import {
+    TableHandler,
+    Datatable,
+    ThSort,
+    ThFilter,
+    Pagination,
+    RowsPerPage,
+    Th,
+    Search,
+    type State,
+  } from '@vincjo/datatables/server'
+
+  import type { PageData } from './$types'
+  import EditPermissions from './EditPermissions.svelte'
+    import EditRole from './EditRole.svelte'
+
+  let { data }: { data: PageData } = $props()
+
+  const filters = new SSRFilters()
+
+  const table = new TableHandler(data.rows, {
+    rowsPerPage: 10,
+    totalRows: data.count,
+  })
+
+  table.setPage(Number(filters.get('page')) || 1)
+  table.load(async s => {
+    console.log(s)
+    filters.fromState(s)
+    await $navigating?.complete
+    return data.rows
+  })
+</script>
+
+<main class="container mx-auto h-full">
+  <Datatable {table}>
+    <!-- {#snippet header()}
+      <Search {table} />
+     
+    {/snippet} -->
+    <table class="table table-zebra">
+      <thead>
+        <tr>
+          <ThSort {table} field="id">ID</ThSort>
+          <ThSort {table} field="username">Nome</ThSort>
+          <ThSort {table} field="email">Email</ThSort>
+          <ThSort {table} field="role">Cargo</ThSort>
+          <ThSort {table} field="meta">Permissões</ThSort>
+          <ThSort {table} field="emailVerified">Email verificado</ThSort>
+          <!-- <ThSort {table} field="created_at">Created At</ThSort> -->
+        </tr>
+        <tr>
+          <Th />
+          <ThFilter {table} field="username" />
+          <ThFilter {table} field="email" />
+          <ThFilter {table} field="role" />
+          <Th/>
+
+          <Th />
+          <!-- <ThFilter {table} field="created_at" /> -->
+          <Th />
+        </tr>
+      </thead>
+      <tbody>
+        {#each table.rows as row}
+          <tr>
+            <td>{row.id}</td>
+            <td><b>{row.username}</b></td>
+            <td><b>{row.email}</b></td>
+            <td><b><EditRole userId={row.id} userName={row.username} userRole={row.role}/></b></td>
+            <td><b><EditPermissions userId={row.id} userName={row.username} userPermissions={row.meta.permissions ?? []}/></b></td>
+            <td><span>{row.emailVerified ? '✅' : '❌'}</span></td>
+            <!-- <td>{row.created_at}</td> -->
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+    {#snippet footer()}
+      <RowsPerPage {table} />
+      <div></div>
+      <Pagination {table} />
+    {/snippet}
+  </Datatable>
+</main>
