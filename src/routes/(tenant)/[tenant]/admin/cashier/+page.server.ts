87af8ce09f87ea75c2230c3d2b688bd0f8082e33
brenin35 <brenoca35@gmail.com>
2024-11-06 14:16:11 -0300

    server

--- src/routes/(tenant)/[tenant]/admin/cashier/+page.server.ts
+++ src/routes/(tenant)/[tenant]/admin/cashier/+page.server.ts
@@ -1,13 +1,10 @@
 import type { PageServerLoad } from './$types'
 
 import { distribuidora } from '$db/controller'
-import { deliveryFeeTable } from '$lib/server/db/schema'
 
 export const load = (async ({locals:{tenantDb}}) => {
   const distribuidoras = await distribuidora(tenantDb!).getCashier()
-  const taxa = await tenantDb!.select().from(deliveryFeeTable)
   return {
     distribuidoras,
-    taxa
   }
 }) satisfies PageServerLoad
