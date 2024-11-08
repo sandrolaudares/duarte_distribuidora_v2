85cf55d541a31518e4af4de6f855d7cad4c7e35a
brenin35 <brenoca35@gmail.com>
2024-11-08 14:33:12 -0300

    criar motoboy tela user

--- src/routes/(tenant)/[tenant]/admin/users/+page.svelte
+++ src/routes/(tenant)/[tenant]/admin/users/+page.svelte
@@ -15,16 +15,18 @@
 
   import type { PageData } from './$types'
   import EditPermissions from './EditPermissions.svelte'
-    import EditRole from './EditRole.svelte'
+  import EditRole from './EditRole.svelte'
   import { onMount } from 'svelte'
   import { trpc } from '$trpc/client'
 
-import { page } from '$app/stores'
+  import { page } from '$app/stores'
   import type { SelectCashier, SelectUser } from '$lib/server/db/schema'
   import { toast } from 'svelte-sonner'
   import EditCaixa from './EditCaixa.svelte'
+  import type { Role } from '$lib/utils/permissions'
 
   let { data }: { data: PageData } = $props()
+  let isOpenModal: HTMLDialogElement | null = null
 
   const filters = new SSRFilters()
 
@@ -40,9 +42,40 @@ import { page } from '$app/stores'
     await $navigating?.complete
     return data.rows
   })
+
+  let newMotoboy = {
+    username: '',
+    email: '',
+    role: 'motoboy' as Role,
+  }
+  let errors = {
+    emailError: '',
+    nameError: '',
+  }
+  let isLoading = $state(false)
+  async function handleInsertMotoboy() {
+    if (newMotoboy.username.length < 3) {
+      errors.nameError = 'Nome do cliente deve conter mais de 3 caracteres'
+      return
+    }
+    try {
+      isLoading=true
+      newMotoboy.role = 'motoboy'
+      await trpc($page).auth.insertUser.mutate(newMotoboy)
+      newMotoboy.username = ''
+      newMotoboy.email = ''
+      toast.success('Motoboy inserido com sucesso!')
+
+    } catch (error: any) {
+      toast.error(error.message)
+      console.error(error.message)
+
+    }
+  }
 </script>
 
-<main class="container mx-auto h-full max-h-[calc(100vh-10vh)]">
+<main class="container mx-auto h-full max-h-[calc(100vh-10vh)] flex flex-col items-end gap-2">
+  <button onclick={() => isOpenModal?.showModal()} class="btn btn-primary">Criar motoboy</button>
   <Datatable {table}>
     <!-- {#snippet header()}
       <Search {table} />
@@ -62,8 +95,8 @@ import { page } from '$app/stores'
           <Th />
           <ThFilter {table} field="username" />
           <ThFilter {table} field="email" />
-          <Th/>
-          <Th/>
+          <Th />
+          <Th />
           <Th />
           <Th />
         </tr>
@@ -74,12 +107,27 @@ import { page } from '$app/stores'
             <td>{row.id}</td>
             <td><b>{row.username}</b></td>
             <td><b>{row.email}</b></td>
-            <td><b><EditRole userId={row.id} userName={row.username} userRole={row.role}/> 
-              {#if row.role === 'caixa'}
-                <EditCaixa userId={row.id} userCashier={row}/>
-              {/if} 
-          </b></td>
-            <td><b><EditPermissions userId={row.id} userName={row.username} userPermissions={row.meta.permissions ?? []}/></b></td>
+            <td>
+              <b>
+                <EditRole
+                  userId={row.id}
+                  userName={row.username}
+                  userRole={row.role}
+                />
+                {#if row.role === 'caixa'}
+                  <EditCaixa userId={row.id} userCashier={row} />
+                {/if}
+              </b>
+            </td>
+            <td>
+              <b>
+                <EditPermissions
+                  userId={row.id}
+                  userName={row.username}
+                  userPermissions={row.meta.permissions ?? []}
+                />
+              </b>
+            </td>
             <td><span>{row.emailVerified ? '✅' : '❌'}</span></td>
           </tr>
         {/each}
@@ -92,3 +140,66 @@ import { page } from '$app/stores'
     {/snippet}
   </Datatable>
 </main>
+
+<dialog class="modal" bind:this={isOpenModal}>
+  <div class="modal-box max-w-lg">
+    <h1 class="text-lg mb-2">Criar motoboy</h1>
+    <div class="flex flex-col gap-2">
+      <label class="input input-bordered flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="h-4 w-4 opacity-70"
+        >
+          <path
+            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+          />
+        </svg>
+        <input
+          type="text"
+          class="grow"
+          placeholder="Nome"
+          bind:value={newMotoboy.username}
+        />
+      </label>
+      <label class="input input-bordered flex items-center gap-2">
+        <input
+          type="text"
+          class="grow"
+          placeholder="Email"
+          bind:value={newMotoboy.email}
+        />
+      </label>
+      <button
+        class="btn btn-primary"
+        disabled={isLoading}
+        onclick={() => {
+          handleInsertMotoboy()
+          setTimeout(()=>{
+            isOpenModal?.close()
+            window.location.reload()
+            isLoading=false
+          },1300)
+        }}
+      >
+        Adicionar
+      </button>
+    </div>
+    <div>
+      {#if errors.nameError}
+        <p class="text-error">
+          {errors.nameError}
+        </p>
+      {/if}
+      {#if errors.emailError}
+        <p class="text-error">
+          {errors.emailError}
+        </p>
+      {/if}
+    </div>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
