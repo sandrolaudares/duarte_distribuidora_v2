9ecdc7ac3c2a912aaf546bdd4935e00bceff3151
brenin35 <brenoca35@gmail.com>
2024-11-11 16:24:21 -0300

    sotck id

--- src/routes/(tenant)/[tenant]/admin/stock/[id]/+page.server.ts
+++ src/routes/(tenant)/[tenant]/admin/stock/[id]/+page.server.ts
@@ -1,15 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { PageServerLoad } from './$types'
 
-import { stock } from '$db/controller'
+import * as schema from '$lib/server/db/schema'
+import {
+  withPagination,
+  withOrderBy,
+  getSQLiteColumn,
+  getOrderBy,
+} from '$lib/server/db/utils'
+import {
+  and,
+  eq,
+  getTableColumns,
+  SQL,
+  count,
+  like,
+  gt,
+  lt,
+  lte,
+  sum,
+  sql,
+  asc,
+  desc,
+} from 'drizzle-orm'
+import { bugReport, customer, stock } from '$lib/server/db/controller'
+import { gte } from 'drizzle-orm'
 import { error } from '@sveltejs/kit'
 
-export const load = (async ({ params,locals:{tenantDb} }) => {
+export const load = (async ({ params,url, locals: { tenantDb } }) => {
   const skuID = params.id
+  const { searchParams } = url
+  const page = Number(searchParams.get('page') ?? 1)
+  const pageSize = Number(searchParams.get('pageSize') ?? 10)
 
-  const estoque = await stock(tenantDb!).getSKUByID(skuID)
+  const sortId = searchParams.get('sort_id')
+  const sortOrder = searchParams.get('sort_order')
 
-  if(!estoque){
-    return error(404, 'SKU não encontrado')
+  const sku = await stock(tenantDb!).getSKUByID(skuID)
+
+  if (!sku) {
+    throw new Error("SKU not found");
+}
+
+  let query = stock(tenantDb!).getTransactionsFromProduto({sku:skuID}).$dynamic()
+
+  if (sortId && sortOrder) {
+    query = withOrderBy(
+      query,
+      getSQLiteColumn(schema.logsTable, sortId),
+      sortOrder,
+    )
   }
-  return { estoque }
+
+  // try {
+  const rows = await withPagination(query, page, pageSize)
+
+  const total = await tenantDb!
+    .select({ count: count() })
+    .from(schema.stockTransactionTable)
+    .where(eq(schema.stockTransactionTable.sku, skuID))
+
+  return { rows: rows ?? [], count: total[0].count,sku }
+  // } catch (err) {
+  //   console.error(err)
+  //   return { rows: [], count: 0, totalSum: 0 }
+  // }
+
+  //TODO: Com trycatch ta dando de tipo na page, arrumar o TRY CATCH
 }) satisfies PageServerLoad
