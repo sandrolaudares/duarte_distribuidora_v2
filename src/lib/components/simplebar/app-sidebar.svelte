39abf4d3f166d598e5def010ace59774ac52d2a2
brenin35 <brenoca35@gmail.com>
2024-11-13 10:53:37 -0300

    sidebar central

---
+++ src/lib/components/simplebar/app-sidebar.svelte
@@ -0,0 +1,84 @@
+<script lang="ts" module>
+	// sample data
+	const data = {
+		versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+		navMain: [
+			{
+				title: "Central",
+				url: "/",
+				items: [
+					{
+						title: "Distribuidoras",
+						url: "/",
+					},
+					{
+						title: "Criar distribuidora",
+						url: "/create",
+					},
+				],
+			},
+		],
+	};
+</script>
+
+<script lang="ts">
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import GalleryVerticalEnd from "lucide-svelte/icons/gallery-vertical-end";
+	import type { ComponentProps } from "svelte";
+	let { ref = $bindable(null), ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
+</script>
+
+<Sidebar.Root bind:ref {...restProps}>
+	<Sidebar.Header>
+		<Sidebar.Menu>
+			<Sidebar.MenuItem>
+				<Sidebar.MenuButton size="lg">
+					{#snippet child({ props })}
+						<a href="##" {...props}>
+							<div
+								class="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg"
+							>
+								<GalleryVerticalEnd class="size-4" />
+							</div>
+							<div class="flex flex-col gap-0.5 leading-none">
+								<span class="font-semibold">Duarte distribuidora</span>
+								<!-- <span class="">v1.0.0</span> -->
+							</div>
+						</a>
+					{/snippet}
+				</Sidebar.MenuButton>
+			</Sidebar.MenuItem>
+		</Sidebar.Menu>
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.Menu>
+				{#each data.navMain as groupItem (groupItem.title)}
+					<Sidebar.MenuItem>
+						<Sidebar.MenuButton class="font-medium">
+							{#snippet child({ props })}
+								<a href={groupItem.url} {...props}>
+									{groupItem.title}
+								</a>
+							{/snippet}
+						</Sidebar.MenuButton>
+						{#if groupItem.items?.length}
+							<Sidebar.MenuSub>
+								{#each groupItem.items as item (item.title)}
+									<Sidebar.MenuSubItem>
+										<Sidebar.MenuSubButton>
+											{#snippet child({ props })}
+												<a href={item.url} {...props}>{item.title}</a>
+											{/snippet}
+										</Sidebar.MenuSubButton>
+									</Sidebar.MenuSubItem>
+								{/each}
+							</Sidebar.MenuSub>
+						{/if}
+					</Sidebar.MenuItem>
+				{/each}
+			</Sidebar.Menu>
+		</Sidebar.Group>
+	</Sidebar.Content>
+	<Sidebar.Rail />
+</Sidebar.Root>
