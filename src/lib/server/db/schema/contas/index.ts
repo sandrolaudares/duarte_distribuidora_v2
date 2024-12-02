7a1f9ef02cbde5fd781c4033c6e833e82faf2f61
brenin35 <brenoca35@gmail.com>
2024-12-02 16:30:50 -0300

    contas a pagar table

--- src/lib/server/db/schema/contas/index.ts
+++ src/lib/server/db/schema/contas/index.ts
@@ -1,14 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import {
-    sqliteTable,
-    text,
-    integer,
-    // customType,
-  } from 'drizzle-orm/sqlite-core'
-  
+  sqliteTable,
+  text,
+  integer,
+  // customType,
+} from 'drizzle-orm/sqlite-core'
+
 import { timestamps } from '../../utils'
-export const customerTable = sqliteTable('cliente', {
-    id: integer('id').primaryKey({ autoIncrement: true }),
-    ...timestamps,
-    
-  })
+import { supplierTable } from '../stock'
+export const contasPagarTable = sqliteTable('cliente', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  ...timestamps,
+  fornecedor_id: integer('fornecedor_id').references(() => supplierTable.id),
+  descricao: text('descricao'),
+  expire_at: integer('updated_at', {
+    mode: 'timestamp',
+  }),
+  valor_conta:integer('valor_conta').notNull()
+})
