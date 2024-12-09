ea3a445c169dd81e137aee36392c50bf6f1ee1d5
brenin35 <brenoca35@gmail.com>
2024-12-09 20:01:16 -0300

    fix chart sotck/sku page

--- src/routes/(tenant)/[tenant]/admin/stock/[id]/+page.svelte
+++ src/routes/(tenant)/[tenant]/admin/stock/[id]/+page.svelte
@@ -33,7 +33,7 @@
   import { tr } from 'date-fns/locale'
   import NoResults from '$lib/components/NoResults.svelte'
   import { format } from 'date-fns'
-  import { goto } from '$app/navigation';
+  import { goto } from '$app/navigation'
   import { icons } from '$lib/utils'
 
   let { data }: { data: PageData } = $props()
@@ -54,24 +54,30 @@
     return data.rows
   })
 
-  let chartData = $derived.by(()=>{
-    data.rows.map(t => ({
-    ...t,
-    created_at: new Date(t.created_at ?? ''),
-  }))
+  let chartData = $derived.by(() => {
+    return data.rows.map(t => ({
+      ...t,
+      created_at: new Date(t.created_at ?? ''),
+    }))
   })
 
-  async function handleDeleteStockItem(sku:string){
+  async function handleDeleteStockItem(sku: string) {
     try {
       await trpc($page).stock.deleteItemStock.mutate(sku)
       toast.success('Deletado com sucesso!')
-      window.location.href = "./"
+      window.location.href = './'
     } catch (error) {
       toast.error('Ocorreu um erro ao deletar!')
     }
   }
+  const calculateYDomain = (data: typeof chartData) => {
+    const minNegative = Math.min(
+      ...data.map(d => (d.quantity < 0 ? d.quantity : 100)),
+    )
+    const maxPositive = Math.max(...data.map(d => d.total_log))
+    return [minNegative, maxPositive]
+  }
 </script>
-
 
 <div class="">
   <div class="container mx-auto flex flex-col gap-5">
@@ -104,67 +110,72 @@
           <div class="stat-value flex justify-center">{estoque.quantity}</div>
         </div>
       </div>
-      <button class="btn btn-circle btn-error w-20 h-20"  onclick={()=>{
-        if(confirm("Tem certeza que deseja deletar?") === true){
-          handleDeleteStockItem(estoque.sku)
-        } else {
-          toast.info("Cancelado")
-        }
-        }}>
+      <button
+        class="btn btn-circle btn-error h-20 w-20"
+        onclick={() => {
+          if (confirm('Tem certeza que deseja deletar?') === true) {
+            handleDeleteStockItem(estoque.sku)
+          } else {
+            toast.info('Cancelado')
+          }
+        }}
+      >
         {@html icons.trash()}
       </button>
     </div>
     <div class="mb-5 flex gap-2">
-      <div
-        class=" h-[70vh] w-full max-w-[50%] overflow-x-auto rounded-box border border-base-300"
-      >
-      <Datatable {table} headless>
-        <!-- {#snippet header()}
+      <div class=" h-[70vh] w-full max-w-[50%] overflow-x-auto">
+        <Datatable {table} headless>
+          <!-- {#snippet header()}
           <Search {table} />
          
         {/snippet} -->
-        <!-- svelte-ignore component_name_lowercase -->
-        <table class="table table-zebra border">
-          <thead>
-            <tr>
-              <Th>Data</Th>
-              <Th>Tipo</Th>
-              <Th>Quantidade</Th>
-              <Th>Preço de custo</Th>
-            </tr>
-          </thead>
-          <tbody>
-            {#each data.rows as row}
+          <!-- svelte-ignore component_name_lowercase -->
+          <table class="table table-zebra rounded-sm border">
+            <thead>
               <tr>
-                <td>{format(new Date(row.created_at || ''),'dd/MM/yyyy')}</td>
-                <td>{row.type}</td>
-                <td>{row.quantity}</td>
-                <td>R${row.cost_price ? (row.cost_price/100).toFixed(2): 0.00}</td>
+                <Th>Data</Th>
+                <Th>Tipo</Th>
+                <Th>Quantidade</Th>
+                <Th>Preço de custo</Th>
               </tr>
-            {/each}
-          </tbody>
-        </table>
-        {#if data.rows.length === 0}
-          <NoResults />
-        {/if}
-        {#snippet footer()}
-          <RowsPerPage {table} />
-          <div></div>
-          <Pagination {table} />
-        {/snippet}
-      </Datatable>
+            </thead>
+            <tbody>
+              {#each data.rows as row}
+                <tr>
+                  <td>
+                    {format(new Date(row.created_at || ''), 'dd/MM/yyyy')}
+                  </td>
+                  <td>{row.type}</td>
+                  <td>{row.quantity}</td>
+                  <td>
+                    R${row.cost_price ? (row.cost_price / 100).toFixed(2) : 0.0}
+                  </td>
+                </tr>
+              {/each}
+            </tbody>
+          </table>
+          {#if data.rows.length === 0}
+            <NoResults />
+          {/if}
+          {#snippet footer()}
+            <RowsPerPage {table} />
+            <div></div>
+            <Pagination {table} />
+          {/snippet}
+        </Datatable>
       </div>
 
-      <div class="w-full max-w-[50%] rounded-box border p-4">
-        <div class="h-[300px] rounded p-4">
+      <div class="w-full max-w-[50%] rounded-sm border p-3">
+        <div class="h-[calc(100%-5vh)] rounded p-4">
           <Chart
             data={chartData}
             x="created_at"
             xScale={scaleBand().padding(0.2)}
             y={['total_log', 'quantity']}
-            yDomain={[0, null]}
+            yDomain={calculateYDomain(chartData)}
             yNice={4}
-            padding={{ left: 16, bottom: 24 }}
+            padding={{ left: 16, bottom: 4 }}
             tooltip={{ mode: 'band' }}
             debug
           >
@@ -172,7 +183,7 @@
               <Axis placement="left" grid rule />
               <Axis
                 placement="bottom"
-                format={d => d.toLocaleString()}
+                format={d => format(d, 'dd/MM/yyyy')}
                 rule
                 tickLabelProps={{
                   rotate: 290,
@@ -188,9 +199,24 @@
               />
             </Svg>
 
-            <!-- <Tooltip header={data => data.created_at} let:data>
-              <TooltipItem label="Quantidade Transacao" value={data.quantity} />
-              <TooltipItem label="Total em estoque no momento" value={data.total_log} />
+            <Tooltip.Root let:data>
+              <Tooltip.Header>
+                {format(data.created_at, 'dd/MM/yyy')}
+              </Tooltip.Header>
+              <Tooltip.List>
+                <Tooltip.Item
+                  label="Quantidade Transacao"
+                  value={data.quantity}
+                />
+                <Tooltip.Item
+                  label="Total em estoque no momento"
+                  value={data.total_log}
+                />
+              </Tooltip.List>
+            </Tooltip.Root>
+            <!-- <Tooltip header={chartData => chartData.created_at} let:data>
+              <TooltipItem label="Quantidade Transacao" value={chartData.quantity} />
+              <TooltipItem label="Total em estoque no momento" value={chartData.total_log} />
             </Tooltip> -->
           </Chart>
         </div>
@@ -198,9 +224,7 @@
     </div>
   </div>
 </div>
-<main class="container mx-auto h-full max-h-[calc(100vh-20vh)]">
-  
-</main>
+
 <style>
   thead {
     background-color: oklch(var(--b1)) !important;
