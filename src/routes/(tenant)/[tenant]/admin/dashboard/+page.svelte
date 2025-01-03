49792478837e6ad84a130b628fa5841d8ae62aea
MatheusCanuto07 <matheushenriquecanuto77@gmail.com>
2025-01-03 16:06:05 -0300

    sync change Andre

--- src/routes/(tenant)/[tenant]/admin/dashboard/+page.svelte
+++ src/routes/(tenant)/[tenant]/admin/dashboard/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   import { DashboardPage } from '$lib/components/dashboard/admin'
-  import type { PageData } from './$types';
-  
-  let { data }: { data: PageData } = $props();
+
+  // import DashboardPage from '$lib/components/dashboard/dashboard.svelte'
+  import type { PageData } from './$types'
+
+  let { data }: { data: PageData } = $props()
 </script>
 
-<h1>Provavelmente essa tela não vai ser acessada, olhar depois</h1>
+<DashboardPage distribuidoras={data.distribuidoras} />
