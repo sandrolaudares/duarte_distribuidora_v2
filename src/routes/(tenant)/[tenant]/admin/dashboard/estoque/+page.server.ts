34884c00ab6725f02304f15b41ffd31b6caab66e
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-01-06 14:11:18 -0300

    feat: queries stock

--- src/routes/(tenant)/[tenant]/admin/dashboard/estoque/+page.server.ts
+++ src/routes/(tenant)/[tenant]/admin/dashboard/estoque/+page.server.ts
@@ -1,50 +1,58 @@
-import type { PageServerLoad } from './$types';
-import { count } from 'drizzle-orm';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import type { PageServerLoad } from './$types'
+import { count, eq, lt } from 'drizzle-orm'
 import * as s from '$db/schema'
 
-export const load = (async ({ locals: { tenantDb : db }, url }) => {
-  
+export const load = (async ({ locals: { tenantDb: db }, url }) => {
   const getTotalItemsStock = db!
-  .select({count : count(s.productItemTable.id)})
-  .from(s.productItemTable)
+    .select({ count: count(s.productItemTable.id) })
+    .from(s.productItemTable)
+
+  const getQuantEntrada = db!
+    .select({ count: count(s.stockTransactionTable.quantity) })
+    .from(s.stockTransactionTable)
+    .where(eq(s.stockTransactionTable.type, 'Entrada'))
+  const getQuantSaida = db!
+    .select({ count: count(s.stockTransactionTable.quantity) })
+    .from(s.stockTransactionTable)
+    .where(eq(s.stockTransactionTable.type, 'Saida'))
 
   return {
     // TODO: Total de itens no estoque
-    totalItemsStock : await getTotalItemsStock,
+    totalItemsStock: await getTotalItemsStock,
     // TODO: Entrada e saida naquele periodo
-    stockInStockOut : 231321,
+    stockInStockOut: 231321,
+    quantSaida: (await getQuantSaida)[0].count,
+    quantEntrada: (await getQuantEntrada)[0].count,
     // TODO: Categoria com menor quantidade de produtos
-    lowestCategory : {
-      name : 'Bebidas',
-      amount : 231321
+    lowestCategory: {
+      name: 'Bebidas',
+      amount: 231321,
     },
     // TODO: Produto que com menor quantidade
-    lowestProduct : {
-      name : 'Cerveja',
-      amount : 231321
+    lowestProduct: {
+      name: 'Cerveja',
+      amount: 231321,
     },
 
     // TODO: Produtos com estoque abaixo do mínimo, não foi especificado qual é esse minimo
     // não sei se é fixo ou se varia de cada produto e é definido no momento do cadastro
-    stockBelowMinimumLevels : [
+    stockBelowMinimumLevels: [
       {
-        name : 'Brahma latão',
-        minimium : 231321,
-        are : 5415
+        name: 'Brahma latão',
+        minimium: 231321,
+        are: 5415,
       },
       {
-        name : 'Skol beats',
-        amount : 7774,
-        are : 5415
+        name: 'Skol beats',
+        amount: 7774,
+        are: 5415,
       },
       {
-        name : 'Carvão de 2 quilos',
-        amount : 231321,
-        are : 5415
-      }
+        name: 'Carvão de 2 quilos',
+        amount: 231321,
+        are: 5415,
+      },
     ],
-    
-
-
-  };
-}) satisfies PageServerLoad;
+  }
+}) satisfies PageServerLoad
