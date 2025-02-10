ad96958dfccb824668c12041c183707312cf5ec4
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-02-10 19:59:58 -0300

    feat: icons

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,8 +1,20 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference lib="webworker" />
+
 import { website } from './lib/config'
+
 try {
   self.addEventListener('fetch', function () {
     return
   })
+  self.addEventListener('install', () => {
+    console.log(`installing service worker`)
+  })
+
+  self.addEventListener('activate', () => {
+    console.log(`activating service worker`)
+  })
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   self.addEventListener('push', function (event: any) {
     const payload = event.data?.text() ?? 'no payload'
