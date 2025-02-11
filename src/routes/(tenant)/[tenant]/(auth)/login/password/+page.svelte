b80d4e0df27261beee372973d5cf1694e7ca3945
brenin35 <brenoca35@gmail.com>
2025-02-10 21:54:40 -0300

    is loading no login

--- src/routes/(tenant)/[tenant]/(auth)/login/password/+page.svelte
+++ src/routes/(tenant)/[tenant]/(auth)/login/password/+page.svelte
@@ -4,7 +4,9 @@
 
   import type { ActionData } from './$types'
 
-  export let form: ActionData
+  let { form }: { form: ActionData } = $props()
+
+  let isLoading = $state(false)
 </script>
 
 <SEO
@@ -15,9 +17,20 @@
 />
 
 <main class="flex min-h-[90vh] items-center justify-center bg-base-200">
-  <div class="w-full max-w-sm rounded-lg p-8 shadow-lg bg-base-100">
+  <div class="w-full max-w-sm rounded-lg bg-base-100 p-8 shadow-lg">
     <h1 class="text-center text-2xl font-semibold">Login com senha</h1>
-    <form method="post" use:enhance class="mt-6 flex flex-col gap-4">
+    <form
+      method="post"
+      use:enhance={() => {
+        isLoading = true
+
+        return async ({ update }) => {
+          await update()
+          isLoading = false
+        }
+      }}
+      class="mt-6 flex flex-col gap-4"
+    >
       <div>
         <label for="email" class="block text-sm font-medium">Email</label>
         <input
@@ -28,9 +41,7 @@
         />
       </div>
       <div>
-        <label for="password" class="block text-sm font-medium">
-          Senha
-        </label>
+        <label for="password" class="block text-sm font-medium">Senha</label>
         <input
           class="input input-bordered mt-1 w-full"
           type="password"
@@ -38,14 +49,18 @@
           id="password"
         />
       </div>
-      <button class="btn btn-primary mt-4 w-full">continuar</button>
+      <button class="btn btn-primary mt-4 w-full" disabled={isLoading}>
+        {#if isLoading}
+          Carregando...
+        {:else}
+          Continuar
+        {/if}
+      </button>
       <p class=" mt-2 text-center text-sm">{form?.message ?? ''}</p>
     </form>
     <p class="mt-4 text-center text-sm">
       Não tem uma conta?
-      <a href="/signup" class="text-primary hover:underline">
-        Crie agora!
-      </a>
+      <a href="/signup" class="text-primary hover:underline">Crie agora!</a>
     </p>
 
     {#if form?.message}
