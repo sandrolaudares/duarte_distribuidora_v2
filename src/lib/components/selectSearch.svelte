278bdeb376c2c632e4bb30fc125f8a39e08b92ab
brenin35 <brenoca35@gmail.com>
2025-02-13 16:25:57 -0300

    Change folder select search component

--- src/lib/components/selectSearch.svelte
+++ src/lib/components/selectSearch.svelte
@@ -7,24 +7,30 @@
 
 <script lang="ts" generics="T">
   import Check from 'lucide-svelte/icons/check'
+  import { Trash } from 'lucide-svelte';
   import ChevronsUpDown from 'lucide-svelte/icons/chevrons-up-down'
   import { tick } from 'svelte'
   import * as Command from '$lib/components/ui/command/index.js'
   import * as Popover from '$lib/components/ui/popover/index.js'
   import { Button } from '$lib/components/ui/button/index.js'
   import { cn } from '$lib/utils'
+  import Separator from './ui/separator/separator.svelte'
+
+  interface Props {
+    options: T[]
+    config: SelectItem<T>
+    value: string
+    placeholder?: string
+    onValueChange?: (value: string) => void
+  }
 
   let {
     options,
     config,
     value = $bindable(),
     placeholder,
-  }: {
-    options: T[]
-    config: SelectItem<T>
-    value: string
-    placeholder: string
-  } = $props()
+    onValueChange,
+  }: Props = $props()
 
   let open = $state(false)
   //   let value = $state('')
@@ -64,11 +70,29 @@
     {/snippet}
   </Popover.Trigger>
   <Popover.Content class="w-[200px] p-0">
-    <Command.Root>
+    <Command.Root
+      onValueChange={() => {
+        if (onValueChange) {
+          onValueChange(value)
+        }
+      }}
+    >
       <Command.Input placeholder={`Pesquise ${placeholder}...`} />
       <Command.List>
         <Command.Empty>Nenhum encontrado.</Command.Empty>
         <Command.Group>
+          <Command.Item
+            value=""
+            onSelect={() => {
+              value = ''
+              closeAndFocusTrigger()
+            }}
+          >
+            <Check
+              class={cn('mr-2 size-4', value !== '' && 'text-transparent')}
+            />Nenhum
+          </Command.Item>
+           <Separator class="my-2 bg-base-300" />
           {#each options as option}
             <Command.Item
               value={config.label(option)}
@@ -86,6 +110,7 @@
               {config.label(option)}
             </Command.Item>
           {/each}
+          
         </Command.Group>
       </Command.List>
     </Command.Root>
