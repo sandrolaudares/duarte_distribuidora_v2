2d70eb14a10d5b84d4b49e1c7797238c1ff70520
brenin35 <brenoca35@gmail.com>
2025-03-10 09:37:53 -0300

    custom Th filter

---
+++ src/lib/components/datatable/CustomThFilter.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  import type { Check, Field, TableHandlerInterface } from '@vincjo/datatables'
+
+  type T = $$Generic<Row>
+  type Props = {
+    table: TableHandlerInterface<T>
+    field: Field<T>
+    check?: Check
+  }
+  let { table, field, check = undefined }: Props = $props()
+
+  const filter = table.createFilter(field, check)
+</script>
+
+<th>
+  <input
+    type="text"
+    placeholder={table.i18n.filter}
+    spellcheck="false"
+    bind:value={filter.value}
+    oninput={() => filter.set()}
+  />
+</th>
+
+<style>
+  th {
+    border-bottom: 1px solid var(--grey, #e0e0e0);
+  }
+  input {
+    max-width: 150px;
+    height: 24px;
+    border: none;
+    text-align: left;
+    padding: 0 20px;
+    background: inherit;
+    outline: none;
+    border-radius: 0;
+    font-size: 14px;
+    color: var(--font-grey, #757575);
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  input::placeholder {
+    color: var(--grey, #bdbdbd);
+    font-size: 13px;
+  }
+  input:focus {
+    outline: none;
+    border: none;
+  }
+</style>
