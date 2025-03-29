4a26ef481ecefc157a3db92283b4193b0b891de0
brenin35 <brenoca35@gmail.com>
2025-03-29 11:28:53 -0300

    unique name table categoria

--- src/lib/stores/user.ts
+++ src/lib/stores/user.ts
@@ -1,20 +1,19 @@
 import { writable, type Writable } from 'svelte/store'
 import { getContext, setContext } from 'svelte'
-
-import type { User } from 'lucia'
+import type { SelectUser } from '$lib/server/db/schema'
 
 const USER_STORE_KEY = 'user'
 
-function createUserStore(user: User | null = null) {
-  const base = writable<User | null>(user)
+function createUserStore(user: SelectUser | null = null) {
+  const base = writable<SelectUser | null>(user)
   return base
 }
 
-export function createUserContext(user: User | null = null) {
+export function createUserContext(user: SelectUser | null = null) {
   const store = createUserStore(user)
   return setContext(USER_STORE_KEY, store)
 }
 
-export function getUserContext(): Writable<User | null> {
+export function getUserContext(): Writable<SelectUser | null> {
   return getContext(USER_STORE_KEY)
 }
