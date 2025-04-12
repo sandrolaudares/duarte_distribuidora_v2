01cfba4edbee40ed5a0d8bdc6da132c55ec17d00
brenin35 <brenoca35@gmail.com>
2025-04-12 17:36:35 -0300

    Tela caixa mostrando quem está nele

--- src/routes/(tenant)/[tenant]/admin/cashier/+page.server.ts
+++ src/routes/(tenant)/[tenant]/admin/cashier/+page.server.ts
@@ -1,9 +1,19 @@
 import type { PageServerLoad } from './$types'
 
-import { distribuidora } from '$db/controller'
+import { cashierTable, userTable } from '$lib/server/db/schema'
+import { eq } from 'drizzle-orm'
 
 export const load = (async ({locals:{tenantDb}}) => {
-  const distribuidoras = await distribuidora(tenantDb!).getCashier()
+
+  const distribuidoras = await tenantDb!.select({
+    id: cashierTable.id,
+    created_at: cashierTable.created_at,
+    name: cashierTable.name,
+    currency: cashierTable.currency,
+    status: cashierTable.status,
+    user_in: userTable.username,
+  }).from(cashierTable).leftJoin(userTable, eq(cashierTable.user_in, userTable.id))
+
   return {
     distribuidoras,
   }
