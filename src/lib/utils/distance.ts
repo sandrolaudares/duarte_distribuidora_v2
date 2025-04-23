164e3a50b06d3427c653f0ff8060f843e5571eba
brenin35 <brenoca35@gmail.com>
2025-04-23 11:26:16 -0300

    get distance/taxa entrega util

--- src/lib/utils/distance.ts
+++ src/lib/utils/distance.ts
@@ -1,3 +1,8 @@
+import { page } from "$app/state"
+import type { SelectAddress } from "$lib/server/db/schema"
+import { trpc } from "$trpc/client"
+import { toast } from "svelte-sonner"
+
 export async function geocodeAddress(address: string) {
   console.log('Geocodificando ' + address)
   try {
@@ -37,4 +42,31 @@ export function getDistanceFromLatLonInKm(
         Math.sin(dLng / 2),
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
   return R * c * 1000
+}
+
+export async function getDistance(endereco:SelectAddress | undefined,fee:number) {
+  let distance = 0
+  let taxaEntrega = 0
+    if (
+      endereco?.distance != null &&
+      endereco?.distance > 0
+    ) {
+      distance = endereco.distance
+    } else if (endereco) {
+      distance = await trpc(page).customer.calculateDistance.mutate({
+        number: endereco.number,
+        bairro: endereco.neighborhood,
+        street: endereco.street,
+        city: endereco.city,
+        state: endereco.state,
+        cep: endereco.cep,
+        country: endereco.country,
+      })
+    }
+    taxaEntrega = (distance / 1000) * (fee / 100)
+    taxaEntrega *= 100
+    taxaEntrega = Math.round(taxaEntrega / 100) * 100
+    toast.success('Distancia: ' + (distance / 1000).toFixed(2) + 'km')
+
+    return taxaEntrega
 }
